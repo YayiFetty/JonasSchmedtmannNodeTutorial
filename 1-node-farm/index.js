1a099b2fbbd84b04dfcc71070d66096d9cb4cca2
Yayi <yayinasir12@gmail.com>
2025-02-02 15:09:46 +0100

    query

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -65,28 +65,72 @@ const url = require("url")
 // turn it to top level code
 // use synchronouse because we only want to read it once 
 
-const tempOverview = fs.readFile(`${__dirname}/templates/template-overview.html`, 'utf-8');
-const tempCard = fs.readFile(`${__dirname}/templates/template-card.html`, 'utf-8');
-const tempProduct = fs.readFile(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 const data= fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
     const dataObj = JSON.parse(data);
 
-const Server = http.createServer((req, res) => {
-    const pathName = req.url;
 
+    // const replaceTemplate = (temp, product) => {  
+    //     const placeholders = {  
+    //       '{%PRODUCTNAME%}': product.productName,  
+    //       '{%IMAGE%}': product.image,  
+    //       '{%PRICE%}': product.price,  
+    //       '{%FROM%}': product.from,  
+    //       '{%NUTRIENTS%}': product.nutrients,  
+    //       '{%QUANTITY%}': product.quantity,  
+    //     };  
+      
+    //     let output = temp;  
+    //     for (const [placeholder, value] of Object.entries(placeholders)) {  
+    //       output = output.replace(new RegExp(placeholder, 'g'), value);  
+    //     }  
+      
+    //     return output;  
+    //   };  
+
+
+    const replaceTemplate = (temp, product) => {
+        let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+        output = output.replace(/{%IMAGE%}/g, product.image);
+        output = output.replace(/{%PRICE%}/g, product.price);
+        output = output.replace(/{%FROM%}/g, product.from);
+        output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+        output = output.replace(/{%QUANTITY%}/g, product.quantity);
+        output = output.replace(/{%DESCRIPTION%}/g, product.description);
+        output = output.replace(/{%ID%}/g, product.id);
+
+        if(!product.organic) output = output.replace(/{%NOT-ORGANIC}%/g, 'not-organic');
+
+        return output;
+    }
+const Server = http.createServer((req, res) => {
+    const {query, pathname} = url.parse(req.url, true);
+        //const pathName = req.url;
     // for the overview
-    if(pathName === '/' || pathName === '/overview'){
+    if(pathname === '/' || pathname === '/overview'){
         res.writeHead(200, {
             "content-type": "text/html"
         })
-        res.end(tempOverview);
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join("");
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+        // res.end(tempOverview);
+        console.log("output", output)
+         res.end(output);
     }
+
     // for the product page
-    else if(pathName === '/product'){
-        res.end("This is a Product")
+    else if(pathname === '/product'){
+        //console.log(query)
+        res.writeHead(200, {'content-type':'text/html'});
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+        // res.end(tempProduct)
+        res.end(output);
     }
     // for the api
-    else if(pathName === "/api"){
+    else if(pathname === "/api"){
        
             res.writeHead(200, {
                 "content-type": "application/json"
